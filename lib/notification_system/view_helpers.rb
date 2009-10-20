ec5f5bbead971234907bd9b5adf8df0601fa268d
radman <radu.a.vlad@gmail.com>
2009-10-20 15:50:58 -0700

    can pass all form_for params to notification_settings_form_for helper

--- lib/notification_system/view_helpers.rb
+++ lib/notification_system/view_helpers.rb
@@ -1,7 +1,7 @@
 module NotificationSystem
   module ViewHelpers
-    def notification_settings_form_for(user)
-      render 'notification_system/notification_settings_form_for', :user => user
+    def notification_settings_form_for(*form_params)
+      render 'notification_system/notification_settings_form_for', :form_params => form_params
     end
   end  
 end
