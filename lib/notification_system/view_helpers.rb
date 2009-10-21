8e465c7b5c85464cfe549aaef77e20e3be78c036
radman <radu.a.vlad@gmail.com>
2009-10-20 17:33:56 -0700

    bug with notification_setting_form_for, parameters weren't passed correctly to form_for

--- lib/notification_system/view_helpers.rb
+++ lib/notification_system/view_helpers.rb
@@ -1,7 +1,7 @@
 module NotificationSystem
   module ViewHelpers
-    def notification_settings_form_for(*form_params)
-      render 'notification_system/notification_settings_form_for', :form_params => form_params
+    def notification_settings_form_for(record_or_name_or_array, *args)
+      render 'notification_system/notification_settings_form_for', :record_or_name_or_array => record_or_name_or_array, :args => args
     end
   end  
 end
