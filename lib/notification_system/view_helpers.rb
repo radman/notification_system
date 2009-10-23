d6d35b25727b5e6f707f4e8eebb16d318e62a7e2
radman <radu.a.vlad@gmail.com>
2009-10-22 17:58:01 -0700

    changed notification_settings_form_for to just notification_settings; we are no longer using the form; instead we will just allow users to add these fields to their forms, giving them greater flexibility in designing the form

--- lib/notification_system/view_helpers.rb
+++ lib/notification_system/view_helpers.rb
@@ -1,6 +1,6 @@
 module NotificationSystem
   module ViewHelpers
-    def notification_settings_form_for(record_or_name_or_array, *args)
+    def notification_settings
       groups = {}
       
       Notification.subscribable_types.each do |subscribable_type|
@@ -8,10 +8,7 @@ module NotificationSystem
         groups[subscribable_type.group] << subscribable_type
       end
       
-      render 'notification_system/notification_settings_form_for', 
-        :record_or_name_or_array => record_or_name_or_array, 
-        :args => args,
-        :groups => groups
+      render 'notification_system/notification_settings', :groups => groups
     end
   end  
 end
--- spec/helper/helpers_spec.rb
+++ spec/helper/helpers_spec.rb
@@ -3,15 +3,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe "Helpers" do
   
   describe "ActionView::Base" do
-    it "should have a notification_settings_form_for instance method" do
-      ActionView::Base.instance_methods.should include('notification_settings_form_for')
+    it "should have a notification_settings instance method" do
+      ActionView::Base.instance_methods.should include('notification_settings')
     end
   end
-  
-  # describe "notification_settings_form_for" do        
-  #   it "should return Radu was here" do
-  #     ClassWithHelpers.new.notification_settings_form_for.should == 'radu was here'
-  #   end    
-  # end
   
 end
