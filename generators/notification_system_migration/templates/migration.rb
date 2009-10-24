3676907a8f8071671d1d440fead4f0a49fd92b41
radman <radu.a.vlad@gmail.com>
2009-10-24 15:03:44 -0700

    reorganized lines in migration.rb

--- generators/notification_system_migration/templates/migration.rb
+++ generators/notification_system_migration/templates/migration.rb
@@ -1,17 +1,29 @@
 class AddNotificationSystem < ActiveRecord::Migration
   def self.up
     create_table :events do |t|
-      t.string :type, :source_type, :data
-      t.integer :source_id
+      t.string :type
+      
+      t.string  :source_type
+      t.ingeger :source_id
+      
+      t.string  :data
+
       t.timestamps
     end
     
     create_table :notifications do |t|
-      t.integer :recipient_id, :event_id
-      t.integer :interval, :default => 0
-      t.datetime :date, :sent_at, :recurrence_end_date
-      t.string :type
-      t.timestamps
+      t.string    :type
+
+      t.integer   :recipient_id
+      t.integer   :event_id
+      
+      t.datetime  :date
+      t.datetime  :sent_at
+      
+      t.integer   :interval, :default => 0
+      t.datetime  :recurrence_end_date
+      
+      t.timestamps      
     end
     
     change_table :users do |t|
