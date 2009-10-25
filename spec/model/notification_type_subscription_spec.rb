d3eadbe883f3a586629697667f21b3016dd55606
radman <radu.a.vlad@gmail.com>
2009-10-24 22:32:44 -0700

    cleaned up notification type subscription spec

---
+++ spec/model/notification_type_subscription_spec.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe 'NotificationTypeSubscription' do
+  it 'should be invalid without a subscriber' do
+    s = NotificationTypeSubscription.make_unsaved :subscriber => nil
+    s.save
+    s.errors.on(:subscriber).should include('can\'t be blank')
+  end
+  
+  it 'should be invalid without a notification type' do
+    s = NotificationTypeSubscription.make_unsaved :notification_type => nil
+    s.save
+    s.errors.on(:notification_type).should include('can\'t be blank')
+  end
+  
+  it 'should be invalid if notification_type references Notification' do
+    s = NotificationTypeSubscription.make_unsaved :notification_type => 'Notification'
+    s.save
+    s.errors.on(:notification_type).should include('must reference a subclass of Notification')
+  end
+  
+  it 'should be invalid if notification_type doesn\'t reference a subclass of notification' do
+    s = NotificationTypeSubscription.make_unsaved :notification_type => 'Blah'
+    s.save
+    s.errors.on(:notification_type).should include('must reference a subclass of Notification')
+  end
+  
+  it 'should be valid if a subscriber and a notification type that\'s a subclass of Notification are specified' do
+    s = NotificationTypeSubscription.make :notification_type => 'RandomNotification', :subscriber => User.create!
+    s.should be_valid
+  end  
+end
