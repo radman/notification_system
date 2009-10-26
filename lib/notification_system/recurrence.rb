a495bb35b2dcb80ea331ea8d9ee01ec00a5b5c2d
radman <radu.a.vlad@gmail.com>
2009-10-26 14:34:07 -0700

    added [](index) accessor to recurrences; so that I can easily find x-th occurrence

--- lib/notification_system/recurrence.rb
+++ lib/notification_system/recurrence.rb
@@ -1,7 +1,13 @@
 module NotificationSystem
+  # A recurrence represents a set of fixed times (and thus requires a start date as a point of reference)
   class Recurrence < ActiveRecord::Base
-    validates_presence_of :interval
+    validates_presence_of :interval, :starts_at
     validate :interval_is_at_least_zero
+       
+    def [](index)
+      return nil if index < 0
+      (occurs_at = starts_at + index * interval) > ends_at ? nil : occurs_at
+    end
     
     private      
   
--- spec/model/recurrence_spec.rb
+++ spec/model/recurrence_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe 'Recurrence' do
   it 'should be invalid without an interval' do
-    recurrence = Recurrence.make_unsaved
+    recurrence = Recurrence.make_unsaved :interval => nil
     recurrence.save
     recurrence.errors.on(:interval).should include('can\'t be blank')
   end
@@ -11,10 +11,42 @@ describe 'Recurrence' do
     recurrence = Recurrence.make_unsaved :interval => -7
     recurrence.save
     recurrence.errors.on(:interval).should include('must be greater than zero')
+  end
+  
+  it 'should be invalid without starts_at' do
+    recurrence = Recurrence.make_unsaved :starts_at => nil
+    recurrence.save
+    recurrence.errors.on(:starts_at).should include('can\'t be blank')
   end  
   
-  it 'should be valid with a positive non-zero interval' do
-    recurrence = Recurrence.make_unsaved :interval => 1
+  it 'should be valid with a positive non-zero interval and with starts_at' do
+    recurrence = Recurrence.make_unsaved :interval => 1, :starts_at => Time.now
     recurrence.should be_valid
-  end  
+  end
+  
+  describe '[](index) instance method' do
+    before(:all) do
+      @recurrence = Recurrence.make :interval => 1.day, :starts_at => Time.now, :ends_at => Time.now + 10.days
+    end
+    
+    it 'should return 1st occurrence if index = 0' do
+      @recurrence[0].should == @recurrence.starts_at
+    end
+
+    it 'should return 8th occurrence if index = 7' do
+      @recurrence[7].should == @recurrence.starts_at + 7 * @recurrence.interval
+    end
+    
+    it 'should return 11th occurrence if index = 10' do
+      @recurrence[10].should == @recurrence.starts_at + 10 * @recurrence.interval
+    end
+    
+    it 'should return nil if index < 0' do
+      @recurrence[-1].should be_nil
+    end
+
+    it 'should return nil if the specified occurrence is past the ends_at date' do
+      @recurrence[11].should be_nil
+    end
+  end
 end
