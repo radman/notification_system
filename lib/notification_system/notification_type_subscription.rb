c113a9ccb1c61f0c76bd2d9ecf56d03b46d8135e
radman <radu.a.vlad@gmail.com>
2009-10-27 18:09:58 -0700

    settings form is working.. although it's not refined at all atm

--- lib/notification_system/notification_type_subscription.rb
+++ lib/notification_system/notification_type_subscription.rb
@@ -12,19 +12,20 @@ module NotificationSystem
     end
         
     # something doesn't seem right about this; mainly that it's only used for recurrent subscriptions
-    # UNTESTED (except via integration tests)
     has_many :notifications, 
       :primary_key => 'notification_type', 
       :foreign_key => 'type', 
       :conditions => ['recipient_id = #{self.send(:subscriber_id)}'], # delay evaluation of #{} by putting it in single quotes
       :class_name => 'NotificationSystem::Notification'
-    
+
+    # UNTESTED    
     def self.create_scheduled_notifications
       all.each do |subscription|
         subscription.create_scheduled_notifications
       end
     end
-    
+
+    # UNTESTED (except via integration tests)    
     def create_scheduled_notifications
       return unless recurrence
       
@@ -36,7 +37,8 @@ module NotificationSystem
         x += 1
       end
     end
-    
+
+    # UNTESTED (but should be a private method anyway)    
     def create_notification_for_date(date)
       notification_type.constantize.create! :recipient => subscriber, :date => date      
     end
--- lib/notification_system/user_extension.rb
+++ lib/notification_system/user_extension.rb
@@ -4,16 +4,61 @@ module NotificationSystem
       base.class_eval do
         has_many  :notification_type_subscriptions, 
                   :class_name => 'NotificationSystem::NotificationTypeSubscription', 
-                  :foreign_key => 'subscriber_id'
+                  :foreign_key => 'subscriber_id'        
       end
     end
     
     def wants_notification?(notification)
       notification.recipient == self && (!notification.class.subscribable? || is_subscribed_to_notification_type?(notification.type))
     end
-    
+
     def is_subscribed_to_notification_type?(notification_type)
       notification_type_subscriptions.exists?(:notification_type => notification_type.to_s)
+    end
+
+    # Example Params:
+    # --- !map:HashWithIndifferentAccess 
+    # CoachingSessionRescheduledNotification: !map:HashWithIndifferentAccess 
+    #   is_subscribed: "true"
+    # NewCommentOnYourArticleNotification: !map:HashWithIndifferentAccess 
+    #   is_subscribed: "true"
+    # ClientActivityNotification: !map:HashWithIndifferentAccess 
+    #   is_subscribed: "true"
+    #   recurrence: !map:HashWithIndifferentAccess 
+    #     starts_at: Tue Oct 27 08:38:05 UTC 2009
+    #     ends_at: Tue Oct 27 09:08:05 UTC 2009
+    #     interval: "120"
+    
+    # TODO: UNTESTED (because i still can't test subscribable_types)
+    # validations also need to be tested (e.g. on recurrences)
+    # HARD: typed values should be saved somehow (or invalid values should be disallowed by design (i.e. dont use textboxes))
+    def notification_types=(params)
+      NotificationSystem::Notification.subscribable_types.each do |subscribable_type|
+        type_params = params[subscribable_type.to_s]
+        recurrence_params = type_params && type_params[:recurrence]
+        
+        if type_params && type_params[:is_subscribed] == "true"
+          # create subscription if one does not exist
+          unless self.is_subscribed_to_notification_type?(subscribable_type)
+            self.notification_type_subscriptions << NotificationTypeSubscription.new(:notification_type => subscribable_type.to_s)
+          end
+        
+          if recurrence_params && recurrence_params[:starts_at] && recurrence_params[:interval]
+            subscription = self.notification_type_subscriptions.find_by_notification_type(subscribable_type.to_s)            
+
+            if subscription.recurrence
+              subscription.recurrence.update_attributes(recurrence_params)
+            else
+              subscription.update_attributes(:recurrence => NotificationSystem::Recurrence.create(recurrence_params))
+            end
+          end
+        else
+          # delete subscription if one exists
+          if self.is_subscribed_to_notification_type?(subscribable_type)
+            self.notification_type_subscriptions.find_by_notification_type(subscribable_type.to_s).delete # TODO: this should destroy associated recurrence as well (if it exists)
+          end  
+        end
+      end
     end
   end
 end
