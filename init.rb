93e02d2a3849c2e148dee5718a38f1ddcbcf4a06
radman <radu.a.vlad@gmail.com>
2009-10-27 20:15:00 -0700

    load notifications & events into memory on init

--- init.rb
+++ init.rb
@@ -9,3 +9,7 @@ ActiveSupport::Dependencies.load_paths << notifications_path
 
 ActiveSupport::Dependencies.load_once_paths << events_path
 ActiveSupport::Dependencies.load_once_paths << notifications_path
+
+# Load notifications and events into memory
+Dir[File.join(events_path, '*')].each { |event_file| require event_file }
+Dir[File.join(notifications_path, '*')].each { |notification_file| require notification_file }
